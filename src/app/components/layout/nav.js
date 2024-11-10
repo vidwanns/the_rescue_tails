d37a9cb959f0ca9eee4bd8c52de82f8d405f43ea
vidwan <163408880+vidwanns@users.noreply.github.com>
2024-11-10 20:01:05 +0530

    nav

--- src/app/components/layout/nav.js
+++ src/app/components/layout/nav.js
@@ -5,23 +5,41 @@ import '../../styles/component/layout/nav.css'; // Updated CSS import
 const Nav = ({ isOpen, closeMenu }) => {
   return (
     <motion.div
-      className="custom-mobile-nav" // Updated unique class name
+      className="custom-mobile-nav"
       initial={{ opacity: 0, y: -100 }}
       animate={{ opacity: isOpen ? 1 : 0, y: isOpen ? 0 : -100 }}
       transition={{ duration: 0.3, ease: "easeInOut" }}
       style={{ display: isOpen ? "block" : "none" }}
     >
+
+<div className="header_logo_nav">
+          <img src="/images/header/logo.svg" alt="The Rescue Tails Logo" />
+        </div>
       <div className="custom-close-icon" onClick={closeMenu}>
         &times;
       </div>
       <nav className="custom-nav-links">
-        <Link href="#home" passHref><div className="custom-nav-link">Home</div></Link>
-        <Link href="#about" passHref><div className="custom-nav-link">About Us</div></Link>
-        <Link href="#how-to-help" passHref><div className="custom-nav-link">How to Help</div></Link>
-        <Link href="#volunteer" passHref><div className="custom-nav-link">Volunteer</div></Link>
-        <Link href="#adopt" passHref><div className="custom-nav-link">Adopt</div></Link>
-        <Link href="#contacts" passHref><div className="custom-nav-link">Contacts</div></Link>
-        <Link href="#say-hello" passHref><div className="custom-nav-link">Say Hello!</div></Link>
+        <div className="custom-nav-link-wrapper">
+          <Link href="#home" passHref><div className="custom-nav-link">Home</div></Link>
+        </div>
+        <div className="custom-nav-link-wrapper">
+          <Link href="#about" passHref><div className="custom-nav-link">About Us</div></Link>
+        </div>
+        <div className="custom-nav-link-wrapper">
+          <Link href="#how-to-help" passHref><div className="custom-nav-link">How to Help</div></Link>
+        </div>
+        <div className="custom-nav-link-wrapper">
+          <Link href="#volunteer" passHref><div className="custom-nav-link">Volunteer</div></Link>
+        </div>
+        <div className="custom-nav-link-wrapper">
+          <Link href="#adopt" passHref><div className="custom-nav-link">Adopt</div></Link>
+        </div>
+        <div className="custom-nav-link-wrapper">
+          <Link href="#contacts" passHref><div className="custom-nav-link">Contacts</div></Link>
+        </div>
+        <div className="custom-nav-link-wrapper">
+          <Link href="#say-hello" passHref><div className="custom-nav-link">Say Hello!</div></Link>
+        </div>
       </nav>
       <div className="custom-buttons">
         <div className="custom-button custom-donate-button">Donate</div>
