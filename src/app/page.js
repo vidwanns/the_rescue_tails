0c7e1f9e3b13990049f10f6de696edff97b03b8e
vidwan <163408880+vidwanns@users.noreply.github.com>
2024-11-13 23:15:38 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -8,8 +8,11 @@ import ThirdSection from "./components/layout/thirdSection";
 import FourthSection from "./components/layout/fourthSection";
 import FifthSection from "./components/layout/fifthSection";
 import SixthSection from "./components/layout/sixthSection";
-import Footer from "./components/layout/footer";
 import FadeInSection from "./components/layout/fadeInSection";
+import dynamic from "next/dynamic";
+
+// Dynamically import Footer with SSR disabled
+const Footer = dynamic(() => import("./components/layout/footer"), { ssr: false });
 
 export default function Home() {
   return (
@@ -21,6 +24,8 @@ export default function Home() {
       <FadeInSection><FourthSection /></FadeInSection>
       <FadeInSection><FifthSection /></FadeInSection>
       <FadeInSection><SixthSection /></FadeInSection>
+      
+      {/* Render Footer only on the client */}
       <Footer />
     </>
   );
