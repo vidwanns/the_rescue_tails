358c183bf89463d84e5e4cdfd9e41e46149bf4a5
vidwan <163408880+vidwanns@users.noreply.github.com>
2024-11-13 23:06:48 +0530

    Update footer.js

--- src/app/components/layout/footer.js
+++ src/app/components/layout/footer.js
@@ -1,21 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../../styles/component/layout/footer.css";
 
 function Footer() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  // useEffect(() => {
-  //   // Check if we're in the client environment before accessing `window`
-  //   if (typeof window !== "undefined") {
-  //     const handleResize = () => setIsMobile(window.innerWidth <= 768);
-  //     handleResize(); // Set initial value on mount
-  //     window.addEventListener("resize", handleResize);
-
-  //     // Cleanup event listener on unmount
-  //     return () => window.removeEventListener("resize", handleResize);
-  //   }
-  // }, []);
-
   const handleDonateClick = () => {
     console.log("Donate button clicked");
   };
